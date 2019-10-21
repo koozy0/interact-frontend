e172d81c1f8dd25c37d9b59eb841039ad70aa649
Koo Zheng Yu <koozhengyu@gmail.com>
2019-10-22 01:56:26 +0800

    Update Home to add padding around searchbox and add icon to search button, update Navbar to change position to sticky

--- src/views/Home.js
+++ src/views/Home.js
@@ -7,8 +7,26 @@ export class Home extends Component {
       <div style={styles.home}>
         <div style={styles.formWrapper}>
           <form style={styles.form}>
-            <input type='text' style={styles.input} />
-            <Link to='/event' style={styles.searchSubmit}>
+            <div
+              style={{
+                ...styles.searchboxAside,
+                ...styles.searchboxAsideLeft,
+              }}
+            >
+              #
+            </div>
+            <input
+              type='text'
+              style={styles.searchbox}
+              placeholder='enter code here'
+            />
+            <Link
+              to='/event'
+              style={{
+                ...styles.searchboxAside,
+                ...styles.searchboxAsideRight,
+              }}
+            >
               <i className='material-icons'>arrow_forward</i>
             </Link>
           </form>
@@ -24,6 +42,8 @@ const styles = {
   home: {
     flex: '1',
     marginTop: '45vh',
+    paddingLeft: '20px',
+    paddingRight: '20px',
   },
   formWrapper: {
     width: '100%',
@@ -31,7 +51,7 @@ const styles = {
     margin: '0 auto',
     position: 'relative',
   },
-  input: {
+  searchbox: {
     width: '100%',
     padding: '16px 48px',
     fontSize: '16px',
@@ -42,16 +62,25 @@ const styles = {
     lineHeight: '24px',
     backgroundColor: '#FAFAFF',
   },
-  searchSubmit: {
-    position: 'absolute',
-    right: '16px',
+  searchboxAside: {
     height: '36px',
     width: '36px',
+    position: 'absolute',
     borderRadius: '18px',
     top: '10px',
     display: 'flex',
     justifyContent: 'center',
     alignItems: 'center',
+  },
+  searchboxAsideLeft: {
+    left: '16px',
+    top: '10px',
+    color: '#3a3335',
+    fontSize: '16px',
+  },
+  searchboxAsideRight: {
+    right: '16px',
+    top: '10px',
     backgroundColor: '#41EAD4',
   },
 };
