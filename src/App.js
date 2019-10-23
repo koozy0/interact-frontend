0eb5f9b74839949e8c4252b2d4a54a5fa770c7d6
Koo Zheng Yu <koozhengyu@gmail.com>
2019-10-24 01:49:04 +0800

    Add socket.io-client, add basic event handlers to handle socket events, convert App to a class based component to add socket event handlers for testing purposes

--- src/App.js
+++ src/App.js
@@ -1,22 +1,32 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import { Navbar } from './layout';
 import { Home } from './views';
 import { Admin } from './admin';
 import { Event } from './event';
+import openSocket from 'socket.io-client';
 
-function App() {
-  return (
-    <Router>
-      <div className='App'>
-        <Navbar />
-        <Route exact path='/' component={Home} />
-        <Route path='/admin' component={Admin} />
-        <Route path='/event' component={Event} />
-      </div>
-    </Router>
-  );
+class App extends Component {
+  componentDidMount() {
+    const socket = openSocket('http://localhost:5000');
+    socket.on('message', message => console.log(message));
+
+    socket.emit('client-message', 'hello this is the client');
+  }
+
+  render() {
+    return (
+      <Router>
+        <div className='App'>
+          <Navbar />
+          <Route exact path='/' component={Home} />
+          <Route path='/admin' component={Admin} />
+          <Route path='/event' component={Event} />
+        </div>
+      </Router>
+    );
+  }
 }
 
 export default App;
