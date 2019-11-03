d0ef068a855e84623772d610f8f26d9d8dbc7342
koozy <koozhengyu@gmail.com>
2019-11-03 13:26:56 +0800

    Update action types

--- src/actions/types.js
+++ src/actions/types.js
@@ -1,4 +1,3 @@
-// TODO: clean up event action/reducers
 // events
 export const CREATE_EVENT = 'CREATE_EVENT'; // create
 export const DELETE_EVENT = 'DELETE_EVENT'; // delete
@@ -6,11 +5,12 @@ export const EVENT_ERROR = 'EVENT_ERROR'; // error
 export const EVENT_LOADING = 'EVENT_LOADING'; // loading
 export const FETCH_EVENT = 'FETCH_EVENT'; // read
 export const FETCH_EVENTS = 'FETCH_EVENTS'; // read
-export const SEARCH_EVENTS = 'SEARCH_EVENTS'; // read
 export const UPDATE_EVENT = 'UPDATE_EVENT'; // update
+
 // errors
 export const DELETE_ERRORS = 'DELETE_ERRORS';
 export const UPDATE_ERRORS = 'UPDATE_ERRORS';
+
 // user
 export const AUTH_ERROR = 'AUTH_ERROR';
 export const LOGIN_FAILURE = 'LOGIN_FAILURE';
@@ -18,6 +18,7 @@ export const LOGIN_SUCCESS = 'LOGIN_SUCCESS';
 export const LOGOUT_SUCCESS = 'LOGOUT_SUCCESS';
 export const USER_LOADED = 'USER_LOADED';
 export const USER_LOADING = 'USER_LOADING';
+
 // questions
 export const CREATE_QUESTION = 'CREATE_QUESTION'; // create
 export const DELETE_QUESTION = 'DELETE_QUESTION'; // delete
@@ -28,19 +29,3 @@ export const QUESTION_ERROR = 'QUESTION_ERROR'; // error
 export const QUESTION_LOADING = 'QUESTION_LOADING'; // loading
 export const QUESTION_UPDATED = 'QUESTION_UPDATED'; // event => updated
 export const UPDATE_QUESTION = 'UPDATE_QUESTION'; // update
-// deprecating
-// events
-// export const CLEAR_EVENTS = 'CLEAR_EVENTS';
-// export const CLEAR_SELECTED = 'CLEAR_SELECTED';
-// export const CREATE_EVENT = 'CREATE_EVENT';
-// export const CREATE_EVENT_SUCCESS = 'CREATE_EVENT_SUCCESS';
-// export const CREATE_QUESTION = 'CREATE_QUESTION';
-// export const CREATE_QUESTION_SUCCESS = 'CREATE_QUESTION_SUCCESS';
-// export const DELETE_EVENT = 'DELETE_EVENT';
-// export const EVENT_ERROR = 'EVENT_ERROR';
-// export const EVENTS_LOADING = 'EVENTS_LOADING';
-// export const GET_EVENT = 'GET_EVENT';
-// export const GET_EVENTS = 'GET_EVENTS';
-// export const SEARCH_EVENTS = 'SEARCH_EVENTS';
-// export const SELECT_EVENT = 'SELECT_EVENT';
-// export const UPDATE_EVENT = 'UPDATE_EVENT';
